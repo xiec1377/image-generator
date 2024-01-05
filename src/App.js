0938ffad416739afbf17dc322e6598be3d02b8ef
Catherine Xie <catherinexie2014@gmail.com>
2024-01-05 03:47:56 -0500

    Edited textbox

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <img class="object-cover rounded-md h-96" src={Sunset} alt="sunset" />
         </div>
         <div>
-          <p class="mt-8 mb-2 text-sm leading-8 text-gray-300">
+          <p class="mt-8 mb-2 text-md leading-8 text-gray-300">
             input a prompt and click ‘enter’ to see your prompt come to life
           </p>
           <div class="mb-6">
@@ -23,11 +23,18 @@ function App() {
           >
             Large input
           </label> */}
-            <input
+            {/* <input
               type="text"
               id="large-input"
-              class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
+              placeholder='pink unicorn eating apple'
+              class="text-wrap block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            /> */}
+            <textarea
+              id="message"
+              rows="4"
+              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="pink unicorn eating apple"
+            ></textarea>
           </div>
           <div class="flex content-center gap-x-8">
             <button class="flex-auto rounded-md bg-transparent border border-indigo-500 border-2 px-3.5 py-2.5 text-sm font-semibold text-indigo-500 shadow-sm hover:border-indigo-400 hover:text-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
